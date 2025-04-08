9b18fa3e64cd03ba7455e4db601b75b3504480da
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-04-08 21:41:42 +0200

    Added encrypt method

---
+++ src/main/java/FileEncryption.java
@@ -0,0 +1,12 @@
+package  main.java;
+
+class  FileEncryption{
+    public static void main(String[] args) {
+           encryptFile("DemoFile");
+    }
+    static  void encryptFile(String filePath){
+
+    }
+}
+
+
