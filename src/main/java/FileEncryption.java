f491d1d2a6b385d6eb185b97332d111192ab3a60
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-04-08 21:44:50 +0200

    Added docs

--- src/main/java/FileEncryption.java
+++ src/main/java/FileEncryption.java
@@ -1,5 +1,18 @@
 package  main.java;
 
+
+/**
+ * TODO:
+    * Description: Created a program that encrypts and 
+    decrypts the contents of a text file using a simple
+    encryption algorithm. Prompting the user to choose
+    between encryption and decryption, and input the
+    file name and path. Encrypt and  decrypt the file
+    accordingly and save the result to a new file.
+
+    Skills: File handling, string manipulation, basic
+    input/output operations.
+ */
 class  FileEncryption{
     public static void main(String[] args) {
            encryptFile("DemoFile");
